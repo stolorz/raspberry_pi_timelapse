6307c404ffc78e488ff06cdc88b865d371195f08
Michal Stolorz <m.stolorz@talkwalker.com>
2023-04-25 13:06:10 +0200

    initial version of timelapse script, taken from explainingcomputers.com

---
+++ timelapse.py
@@ -0,0 +1,19 @@
+#import libraries
+from picamera import PiCamera
+import time
+
+#Set up variables:
+interval = 15 
+frame = 0
+
+#Set up & start camera, & let it settle
+camera = PiCamera()
+camera.resolution = (4056, 3040)
+camera.start_preview()
+time.sleep(2)
+
+while True:
+        camera.capture('/home/pi/Sync/timelapse/ice_%04d.jpg' % (frame))
+        frame = frame + 1
+        time.sleep(interval)
+  
