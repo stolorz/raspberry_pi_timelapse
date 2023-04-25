7223442d35d1eed531eeb83b708a5f49114ffc4a
Michal Stolorz <m.stolorz@talkwalker.com>
2023-04-25 17:02:14 +0200

    correct formating of output filenames

--- timelapse_basic.py
+++ timelapse_basic.py
@@ -4,6 +4,7 @@ import time
 
 #Set up variables:
 interval = 1
+output_folder = '/home/pi/Sync/timelapse/'
 
 
 #Set up & start camera, & let it settle
@@ -17,6 +18,7 @@ iso = 1
 
 while True:
     camera.iso = iso
-    camera.capture('/home/pi/Sync/timelapse/' + time.strftime("%Y%m%d-%H%M%S") + '_iso_'+iso+'.jpg' )
+    output_filename = '{0}/{1}_iso_{2}.jpg'.format(output_folder, time.strftime("%Y%m%d-%H%M%S"), iso)
+    camera.capture( output_filename )
     iso *=2
     time.sleep(interval)
