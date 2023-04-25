38cf09ff4b83d21bc11155e3658050dce136a391
Michal Stolorz <m.stolorz@talkwalker.com>
2023-04-25 13:57:16 +0200

    rollback to picamera1

--- timelapse.py
+++ timelapse.py
@@ -1,25 +1,19 @@
+#import libraries
+from picamera import PiCamera
 import time
-from picamera2 import Picamera2, Preview
 
-picam = Picamera2()
-
-config = picam.create_preview_configuration()
-picam.configure(config)
-
-#picam.start_preview(Preview.QTGL)
-
-interval = 15 
+#Set up variables:
+interval = 15
 frame = 0
 
-picam.start()
+#Set up & start camera, & let it settle
+camera = PiCamera()
+camera.resolution = (4056, 3040)
+camera.start_preview()
 time.sleep(2)
-picam.capture_file("test-python.jpg")
 
 while True:
-        picam.capture_file('/home/pi/Sync/timelapse/ice_%04d.jpg' % (frame))
+        camera.capture('/home/pi/Sync/timelapse/ice_%04d.jpg' % (frame))
         frame = frame + 1
         time.sleep(interval)
-
-picam.close()
-
 
