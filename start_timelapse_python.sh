2f981305fb995ab41d59ffbbee125392dd63593f
Michal Stolorz <michal@stolorz.pl>
2023-04-26 17:15:37 +0200

    log to a txt file

--- start_timelapse_python.sh
+++ start_timelapse_python.sh
@@ -6,7 +6,7 @@ log_folder="/home/pi/Sync/timelapse"
 
 while true
 do
-printf -v log_filename "${log_folder}/log_%(%Y-%m-%d_%H-%M-%S)T" -1
+printf -v log_filename "${log_folder}/log_%(%Y-%m-%d_%H-%M-%S)T.txt" -1
 echo "starting timelapse python script ${counter}" >> "${log_filename}"
 echo -e "current ifconfig:\n" >> "${log_filename}"
 ifconfig >> "${log_filename}"
