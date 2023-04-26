d24957191e7d6918af8fc95243d5daba9ffc758a
Michal Stolorz <m.stolorz@talkwalker.com>
2023-04-26 15:51:46 +0200

    correct formatting secodns in the timestamp

--- start_timelapse_python.sh
+++ start_timelapse_python.sh
@@ -4,10 +4,9 @@ counter=0
 
 log_folder="/home/pi/Sync/timelapse"
 
-
 while true
 do
-printf -v log_filename "${log_folder}/log_%(%Y-%m-%d_%H-%M-%s)T" -1
+printf -v log_filename "${log_folder}/log_%(%Y-%m-%d_%H-%M-%S)T" -1
 echo "starting timelapse python script ${counter}" >> "${log_filename}"
 echo -e "current ifconfig:\n" >> "${log_filename}"
 ifconfig >> "${log_filename}"
