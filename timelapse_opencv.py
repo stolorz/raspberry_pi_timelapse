767f46d888d3c4d28c0f6a91040ff3c722a39839
Michal Stolorz <michal@stolorz.pl>
2023-04-26 16:30:47 +0200

    more experimental code around openCv

--- timelapse_opencv.py
+++ timelapse_opencv.py
@@ -6,21 +6,26 @@ import cv2
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (4056, 3040)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(4056, 3040))
 
 # allow the camera to warmup
 time.sleep(0.1)
+
+counter = 0;
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
-    image = frame.array
+    image = rawCapture.array
+
+    cv2.imwrite("/home/pi/Sync/timelapse/xxx_{0}.jpg".format(counter), image)
+    counter += 1
 
     # show the frame
-    cv2.imshow("Frame", image)
+    # cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
@@ -29,3 +34,4 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
+    time.sleep(10)
